5c5dafffc32073bd64949df553214447ae3a85cb
Tom Black <tom.black06007@gmail.com>
2024-01-01 19:56:09 -0800

    completed part 1 of day1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("./text.txt");
+    let list_of_numbers = create_list_of_numbers(input);
+    let sum = sum_of_number_list(list_of_numbers);
+    println!("The sum of numbers is {:?}", sum);
+}
+
+fn create_list_of_numbers(input: &str) -> Vec<u32> {
+    let list_of_numbers = input
+    .lines()
+    .map(|line| {
+        let mut first_digit = 0;
+        let mut second_digit = 0;
+        for c in line.chars() {
+            if c.is_digit(10) && first_digit == 0 {
+                first_digit = c.to_digit(10).unwrap();
+                second_digit = c.to_digit(10).unwrap();
+            } else if c.is_digit(10) {
+                second_digit = c.to_digit(10).unwrap();
+            }
+        }
+    (first_digit * 10) + second_digit
+    })
+    .collect();
+    return list_of_numbers;
+}
+
+fn sum_of_number_list(list: Vec<u32>) -> u32 {
+    let mut sum = 0;
+    for num in list {
+        sum += num;
+    }
+    sum
 }
