44235dcf66667d49358cfbdc7817b9a44325b87c
Tom Black <tom.black06007@gmail.com>
2024-01-06 21:43:15 -0700

    got part 2 worked out and set up day 2

--- day-1/src/bin/part2.rs
+++ day-1/src/bin/part2.rs
@@ -1,18 +1,17 @@
 fn main() {
-    //let input = include_str!("./text.txt");
-    let input = "two1nine
+    let input = include_str!("./text.txt");
+    /*let input = "two1nine
     eightwothree
     abcone2threexyz
     xtwone3four
     4nineeightseven2
     zoneight234
-    7pqrstsixteen";
+    7pqrstsixteen";*/
     process(input);
 }
 
 fn process(input: &str) -> u32 {
     let output = input.lines().map(process_line).sum::<u32>();
-
     dbg!(&output);
     output
 }
@@ -22,40 +21,40 @@ fn process_line(line: &str) -> u32 {
     let line_iter = std::iter::from_fn(move || {
         let reduced_line = &line[index..];
         let result = if reduced_line.starts_with("one") {
-            index += "one".len();
+            //index += "one".len();
             Some('1')
         } else if reduced_line.starts_with("two") {
-            index += "two".len();
+            //index += "two".len();
             Some('2')
         } else if reduced_line.starts_with("three") {
-            index += "three".len();
+           //index += "three".len();
             Some('3')
         } else if reduced_line.starts_with("four") {
-            index += "four".len();
+            //index += "four".len();
             Some('4')
         } else if reduced_line.starts_with("five") {
-            index += "five".len();
+            //index += "five".len();
             Some('5')
         } else if reduced_line.starts_with("six") {
-            index += "six".len();
+            //index += "six".len();
             Some('6')
         } else if reduced_line.starts_with("seven") {
-            index += "seven".len();
+            //index += "seven".len();
             Some('7')
         } else if reduced_line.starts_with("eight") {
-            index += "eight".len();
+            //index += "eight".len();
             Some('8')
         } else if reduced_line.starts_with("nine") {
-            index += "nine".len();
+            //index += "nine".len();
             Some('9')
         } else {
             let result = reduced_line.chars().next();
-            index += 1;
             result
         };
+        index += 1;
         result
     });
-    dbg!(&line_iter);
+    //dbg!(&line_iter);
     let mut it = line_iter.filter_map(|character| character.to_digit(10));
     let first = it.next().expect("Should be a number");
 
